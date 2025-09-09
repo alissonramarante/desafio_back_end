510c5722138d0e244f908b74c6cb3c304f4ad52d
alissonramarante <alisson.amarante@ccc.ufcg.edu.br>
2025-09-09 00:34:39 -0300

    criando script de automacao

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ O desafio é composto por três partes:
 ### Relacionamentos:
 - Um Benchmark possui 1 ou muitos Controles;
 - Um Controle deve pertencer a apenas 1 Benchmark;
-- Um Contorle possui 0 ou muitos Históricos_Estado;
+- Um Controle possui 0 ou muitos Históricos_Estado;
 - Um Historico_Estado deve pertencer a apenas 1 Controle
 
 ![Banco de Dados MER - Benchmark](questao1/modelo_banco.png)
@@ -26,18 +26,18 @@ O desafio é composto por três partes:
   [Clique aqui!](https://app.brmodeloweb.com/#!/publicview/68bc20e90ec1bb87cf05d89b)
 
 ## Índices básicos
-- **Chaves Primárias (PKs)**: Criar indices nas colunas `id` das tabelas **Benchmark**, **Controle** e **Histórico_estado**. Esses indices permitem localizar registo específico com mais rapizes, conforme o cenário Q1 e Q3.
+- **Chaves Primárias (PKs)**: Criar índices nas colunas `id` das tabelas **Benchmark**, **Controle** e **Histórico_estado**. Esses índices permitem localizar registo específico com mais rapidez, conforme o cenário Q1 e Q3.
 
-- **Chaves Estrangeiras (FKs)**: Criar indices nas colunas `id_benchmark` da tabela **Controle** e `id_controle` na tabela **Historico_Estado**. Esses indices ajuda a acelerar as operações de junção (`JOIN`) entre as tabelas.
+- **Chaves Estrangeiras (FKs)**: Criar índices nas colunas `id_benchmark` da tabela **Controle** e `id_controle` na tabela **Historico_Estado**. Esses índices ajuda a acelerar as operações de junção (`JOIN`) entre as tabelas.
 
-- **Índice Composto**: Criar indice na coluna `date` e `id_controle` na tabela **Historico_Estado**, para otimizar as buscas por um intervalo de tempo, conforme os cenário Q2 e Q3
+- **Índice Composto**: Criar índices na coluna `date` e `id_controle` na tabela **Historico_Estado**, para otimizar as buscas por um intervalo de tempo, conforme os cenário Q2 e Q3
 
 ## Questão 2 — Script em Python automatização de Ambientes Operacionais
-
-Devemos dividir tem 3 Etapas
-- Listar todos os arquivos
-- Remover arquivos com mais de 3 dias
-- Copiar arquivos para outro diretório
+A automação do processo de backup foi implementado em Python [**automacao.py**](questao2/automacao.py).
+O Script realiza as seguintes etapas:
+* **Listar os Arquivos:** Lista todos os arquivos do diretório de origem e salva o resultado em arquivo de log `backupsFrom.log`;
+* **Remover os Arquivos antigos:** Remove os arquivos que foram criados há mais de 3 dias.
+* **Copiar os Arquivos recentes:** Copia os arquivos mais recentes para o diretório de destino e salva o resultado no log `backupsTo.log`.
 
 
 ## Questão 3 — Problema -> Causa -> Solução do Deploy
---
+++ questao2/automacao.py
@@ -0,0 +1,94 @@
+import os
+import shutil
+import datetime
+import time
+
+# Listando os arquivos e salvar no log 
+
+source_dir = '/home/valcann/backupsFrom'
+log_from_file = '/home/valcann/backupsFrom.log'
+
+if not os.path.exists(source_dir):
+
+    print(f"Erro: O diretório de origem {source_dir} não foi encontrado.")
+
+else:
+    with open(log_from_file, 'w') as f:
+
+        f.write('Relatório de Arquivos - backpusFrom.log\n')
+        f.write('-' * 40 + '\n')
+
+        for filename in os.listdir(source_dir):
+
+            file_path = os.path.join(source_dir, filename)
+
+            if os.path.isfile(file_path):
+
+                file_stats = os.stat(file_path)
+                size_in_bytes = file_stats.st_size
+                create_time = datetime.datetime.fromtimestamp(file_stats.st_ctime).strftime('%Y-%m-%d %H:%M:%S')
+                modify_time = datetime.datetime.fromtimestamp(file_stats.st_mtime).strftime('%Y-%m-%d %H:%M:%S')
+
+                log_entry = (
+                    f'Arquivo: {filename}\n'
+                    f'Tamanho: {size_in_bytes} bytes\n'
+                    f'Criado em: {create_time}\n'
+                    f'Última modificação: {modify_time}\n'
+                    f'{"-" * 40}\n'
+                )
+                f.write(log_entry)
+    print(f"Relatório gerado com sucesso em {log_from_file}.")
+
+# Removendo arquivos com mais de 3 dias
+
+days_limit = 3 * 86400
+current_time = time.time()
+
+for filename in os.listdir(source_dir):
+    file_path = os.path.join(source_dir, filename)
+
+    if os.path.isfile(file_path):
+        file_creation_time = os.path.getctime(file_path)
+
+        if (current_time - file_creation_time) > days_limit:
+            try:
+                os.remove(file_path)
+                print(f'Arquivo {filename} removido com sucesso.')
+            except Exception as e:
+                print(f'Erro ao remover o arquivo {filename}: {e}')
+
+# Copiando arquivos com menos de 3 dias para outro diretório e salvando log
+
+
+dest_dir = '/home/valcann/backupsTo'
+log_to_file = '/home/valcann/backupsTo.log'
+
+
+if not os.path.exists(dest_dir):
+    os.makedirs(dest_dir)
+
+with open(log_to_file, 'w') as f:
+    f.write(f'Relatório de Cópia - backupsTo.log\n')
+    f.write('-' * 40 + '\n')
+
+for filename in os.listdir(source_dir):
+    file_path = os.path.join(source_dir, filename)
+    file_creation_time = os.path.getctime(file_path)
+
+    if(current_time - file_creation_time) <= days_limit:
+        try:
+            shutil.copy2(file_path, dest_dir)
+            log_entry = f'Arquivo {filename} copiado com sucesso.\n'
+            f.write(log_entry)
+            print(log_entry.strip())
+        except Exception as e:
+            log_entry = f'Erro ao copiar o arquivo {filename}: {e}\n'
+            f.write(log_entry)
+            print(log_entry.strip())
+
+print(f'Relatório de cópia salvo em "{log_to_file}" com sucesso.')
+       
+
+
+
+
